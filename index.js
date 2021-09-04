fb231139a19db38fa3945e978b6722883ed26ea8
Eric Schleiff <ericschleiff@gmail.com>
2021-09-04 02:07:39 -0500

    path fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const gulp = require('./gulpfile');
-const path = require('path/posix');
+const path = require('path');
 
 const app = express();
 
